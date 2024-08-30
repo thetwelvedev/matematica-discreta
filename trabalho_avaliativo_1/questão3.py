4e42fb3c7fa3356fb06785de56770395b18feb5f
Leonardo Castro <twelve.programming@gmail.com>
2024-08-30 00:18:47 -0400

    Questão 3 em andamento

--- trabalho_avaliativo_1/questão3.py
+++ trabalho_avaliativo_1/questão3.py
@@ -5,3 +5,19 @@ m = 2 -> n(n-1)(2n+1)/6
 m = 3 -> n²(n-1)²/4
 Então a saída tem que ser uma fórmula dependendo do valor de m.
 """
+import sympy as sp
+import os 
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+
+
+
+
+# Exemplo de uso
+m = int(input("Digite o valor da potência: "))
+limpar_tela()
+#sp.pretty_print(formula(m))
