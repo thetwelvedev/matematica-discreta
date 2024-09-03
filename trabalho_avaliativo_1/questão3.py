7f384b8270d04a26c6bf9f3ad5536045a376e1ad
Leonardo Castro <twelve.programming@gmail.com>
2024-09-03 19:06:45 -0400

    Em andamento

--- trabalho_avaliativo_1/questão3.py
+++ trabalho_avaliativo_1/questão3.py
@@ -11,13 +11,19 @@ import os
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def somatorio(j):
-    return sum(range(0, j + 1))
+def formula_PAM4(m):
+    for j in range(0, m+1):
+        a = sum(((-1)**k)*sp.binomial(j,k)*(1-k+j)**m for k in range(0, j+1))
+    return a
 
-
+def formula_PAM5(m):
+    formula_ant = formula_PAM4(m)
+    n = sp.symbols('n')
+    formula = sum(sp.binomial(n,j+1)*formula_ant(j+1) for j in range(0, m+1))
+    return sp.simplify(formula)
 
 
 # Exemplo de uso
 m = int(input("Digite o valor da potência: "))
 limpar_tela()
-#sp.pretty_print(formula(m))
+sp.pretty_print(formula_PAM5(m))
