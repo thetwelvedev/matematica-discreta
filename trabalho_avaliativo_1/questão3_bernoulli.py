df42d3ac3f365920d29a15a4db26a07ad2f02ae1
Leonardo Castro <twelve.programming@gmail.com>
2024-09-05 15:58:01 -0400

    Segunda questão do primeiro trabalho

--- trabalho_avaliativo_1/questão3_bernoulli.py
+++ trabalho_avaliativo_1/questão3_bernoulli.py
@@ -1,8 +1,8 @@
 """Questão 3 - Fórmulas da Soma de inteiros elevado a m 
 Onde temos as seguintes fórmulas:
-m = 1 -> n(n-1)/2
-m = 2 -> n(n-1)(2n+1)/6
-m = 3 -> n²(n-1)²/4
+m = 1 -> n(n+1)/2
+m = 2 -> n(n+1)(2n+1)/6
+m = 3 -> n²(n+1)²/4
 Então a saída tem que ser uma fórmula dependendo do valor de m.
 """
 import sympy as sp
